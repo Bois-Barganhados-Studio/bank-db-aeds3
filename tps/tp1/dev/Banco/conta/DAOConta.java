681e58c8a3a5858bb2e085c2d29afc5a0d5cda16
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-09-19 01:11:39 -0300

    DAConta com sistema de lastId e ler com nome de user

--- tps/tp1/dev/Banco/conta/DAOConta.java
+++ tps/tp1/dev/Banco/conta/DAOConta.java
@@ -1,4 +1,45 @@
 package conta;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
 public class DAOConta {
-    
+
+    private RandomAccessFile dataArq;
+    private int lastId;
+    private Conta conta;
+
+    public DAOConta() throws IOException {
+        dataArq = new RandomAccessFile("../db/conta_banco.db", "rw");
+        conta = null;
+        lastId = dataArq.readInt();
+    }
+
+    public boolean inserir(Conta obj) throws IOException, Exception {
+        return false;
+    }
+
+    public boolean alterar(Conta obj) throws IOException, Exception {
+        return false;
+    }
+
+    public Conta ler(int id) throws IOException {
+        return conta;
+    }
+
+    // para fazer conferência de nomes iguais
+    public Conta ler(String nomeUser) throws IOException {
+        return conta;
+    }
+
+    public boolean delete(int id) throws IOException {
+        return false;
+    }
+
+    public ArrayList<Conta> listar() throws IOException {
+        ArrayList<Conta> lista = new ArrayList<>();
+        return lista;
+    }
+
 }
