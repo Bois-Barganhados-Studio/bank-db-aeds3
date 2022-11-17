deeae284940e251a999bdbf1b39019f5e82e264e
Vinicius Gabriel <viniciusgabriel7711@gmail.com>
2022-11-17 19:39:44 -0300

    Added Huffman compression

---
+++ tps/tp1/dev/Banco/estruturas/Huffman/HuffmanCoding.java
@@ -0,0 +1,224 @@
+package Huffman;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.TreeSet;
+
+/**
+ * @author Vinicius G Santos
+ */
+public class HuffmanCoding {
+    private RandomAccessFile src;
+    private RandomAccessFile output;
+
+    private static final int MAX_MEM_BYTES = 10000;
+    public static final String VERSION = "HuffmanCompressaov0.1";
+
+    HuffmanCoding() {}
+
+    /**
+     * Closes and dereference RandomAccessFile attributes.
+     * @throws IOException IO error caused by RandoAccessFile.
+     */
+    public void close() throws IOException {
+        if (src != null) {
+            src.close();
+            src = null;
+        }
+        if (output != null) {
+            output.close();
+            output = null;
+        }
+    }
+
+    /**
+     * Generates a compression target file based on the given source file.
+     * @param srcFile The source file to be compressed.
+     * @return The target file of the compression.
+     */
+    private File genCompressionFile(File srcFile) {
+        // Adds the compression version
+        return new File(srcFile.getParent() + File.separator + VERSION + srcFile.getName());
+    }
+
+    /**
+     * Reads all the bytes from source file, counts their occurrence amount then returns a TreeSet.
+     * @return A TreeSet of the occurrences and their weights.
+     * @throws IOException IO error caused by RandoAccessFile.
+     */
+    private TreeSet<Node> getOccurrences() throws IOException {
+        if (src == null || src.length() == 0) throw new IOException("Cannot read from source file");
+        Hashtable<Byte, Integer> occurrenceTable = new Hashtable<>();
+        // Rewinds source file pointer
+        src.seek(0);
+        // Reads from file by parts as the full file may not fit entirely in RAM
+        long bytesLeft;
+        byte[] ba = new byte[MAX_MEM_BYTES];
+        for (bytesLeft = src.length(); bytesLeft > MAX_MEM_BYTES; bytesLeft -= MAX_MEM_BYTES) {
+            src.read(ba);
+            for (byte b : ba)
+                // If the byte is not already present in the table adds it with a value 1
+                // else sums 1 to the value already in the table
+                occurrenceTable.merge(b, 1, Integer::sum);
+        }
+        // Reads the remaining bytes in the file
+        if (bytesLeft > 0) {
+            src.read(ba, 0, (int) bytesLeft);
+            for (int i = 0; i < bytesLeft; i++)
+                occurrenceTable.merge(ba[i], 1, Integer::sum);
+        }
+        // Gets the occurrences (keys) in the hashtable
+        ArrayList<Byte> values = new ArrayList<>(occurrenceTable.keySet());
+        // Gets the weights (amount of appearances) is position relative to its occurrence
+        ArrayList<Integer> weights = new ArrayList<>(occurrenceTable.values());
+        // Creates TreeSet with custom ordering
+        TreeSet<Node> occurrences = new TreeSet<>(Node::TreeSetCompareTo);
+        // Inserts in TreeSet
+        for (int i = 0; i < values.size(); i++)
+            occurrences.add(new Node(values.get(i), weights.get(i)));
+        // Rewinds source file pointer
+        src.seek(0);
+
+        return occurrences;
+    }
+
+    /**
+     * Compresses the given source file based on the Huffman coding algorithm.
+     *
+     * @param srcFile The name of the source file.
+     * @throws IOException If the File is not found or IO error caused by RandomAccessFile.
+     */
+    public File compress(File srcFile) throws IOException {
+        // Close possible open raf attributes then prep new raf
+        close();
+        src = new RandomAccessFile(srcFile, "r");
+        File outFile = genCompressionFile(srcFile);
+        output = new RandomAccessFile(outFile, "rw");
+        // Gets occurrences and their weights
+        // then saves it in the output file
+        TreeSet<Node> occurrences = getOccurrences();
+        byte[] ba = HuffmanTree.toByteArray(occurrences);
+        output.writeInt(ba.length);
+        output.write(ba);
+        // Creates the tree then generates encode table containing
+        // the occurrences and their binary representation string
+        HuffmanTree tree = new HuffmanTree(new TreeSet<>(occurrences));
+        Hashtable<Byte, String> encodeTable = tree.genEncodeTable();
+        // Calculates the length of the data compressed
+        long bitLen = getEncodedBitLen(occurrences, encodeTable);
+        // Writes in the file 'x' first bits to be ignored
+        byte fillerBits = (byte)(bitLen % 8);
+        output.write(fillerBits);
+        // Creates string builder to append the binary representation
+        StringBuilder bin = new StringBuilder();
+        // Inserts the first 'x' filler bits
+        bin.append("0".repeat(fillerBits));
+        // Reads from file by parts as the full file may not fit entirely in RAM
+        long len;
+        ba = new byte[MAX_MEM_BYTES];
+        // Read, encode, write loop
+        for (len = src.length(); len > MAX_MEM_BYTES; len -= MAX_MEM_BYTES) {
+            src.read(ba);
+            output.write(encode(encodeTable, ba, len, bin));
+        }
+        // Read, encode, write the remaining bytes
+        if (len > 0) {
+            src.read(ba, 0, (int) len);
+            output.write(encode(encodeTable, ba, len, bin));
+        }
+        // Closes raf attributes, restart branch ID counter
+        close();
+        Node.restartCount();
+        // Return string full path of the compressed file
+        return outFile;
+    }
+
+    /**
+     * Calculates the length in bits of the compressed data.
+     * @param occurrences TreeSet with the occurrence and its amount.
+     * @param encodeTable A hashtable containing each occurrence and its binary code as a string generated from the Huffman tree.
+     * @return The length in bits of the compressed data.
+     */
+    private long getEncodedBitLen(TreeSet<Node> occurrences, Hashtable<Byte, String> encodeTable) {
+        long bitLen = 0;
+        for (Node it : occurrences) {
+            // Length of the binary representation string
+            int len = encodeTable.get(it.getValue()).length();
+            bitLen += ((long) it.getWeight() * len);
+        }
+        return bitLen;
+    }
+
+    /**
+     * Encodes the given byte array using the given encode hash table
+     * that contains the encoded binary (generated from the Huffman tree)
+     * as a string for each occurrence of the byte.
+     * @param encodeTable A hashtable containing each occurrence and its binary code as a string generated from the Huffman tree.
+     * @param ba The byte array to be encoded.
+     * @param len The length of the byte array to be used.
+     * @param bin A string builder to append the binary representation of the encoded data.
+     */
+    private byte[] encode(Hashtable<Byte, String> encodeTable, byte[] ba, long len, StringBuilder bin) throws IOException {
+        // For each byte up to 'len' position in the byte array
+        // appends its string binary representation to the string builder
+        for (int i = 0; i < (int) len; i++)
+            bin.append(encodeTable.get(ba[i]));
+        // Trying to free some memory
+        ba = null;
+        System.gc();
+        // Keeps reading and encoding new bytes util the length is divisible by 8
+        // so that it won't produce gaps in the compressed file
+        while ((bin.length() % 8) != 0) {
+            bin.append(encodeTable.get(src.readByte()));
+        }
+        // Converts string binary representation to actual binary
+        // then returns the encoded byte array
+        String aux = bin.toString();
+        bin.delete(0, bin.length());
+        return new BigInteger(aux, 2).toByteArray();
+    }
+
+    /**
+     * Decompresses the given source file based on the Huffman coding algorithm.
+     *
+     * @param srcFile The name of the source file.
+     * @throws IOException If the File is not found or IO error caused by RandomAccessFile.
+     */
+    public void Decompress(File srcFile) throws IOException {
+        close();
+        src = new RandomAccessFile(srcFile, "r");
+        output = new RandomAccessFile(getDecompressionFile(srcFile), "rw");
+
+        byte[] ba = new byte[src.readInt()];
+        src.read(ba);
+        Hashtable<String, Byte> decodeTable = new HuffmanTree(ba).getDecodeTable();
+
+        // TODO - Decompression
+    }
+
+    /**
+     * Gets the decompression target file
+     * based on the given compressed file.
+     * @param srcFile The source (compressed) file.
+     * @return A decompression target file.
+     */
+    public File getDecompressionFile(File srcFile) throws IOException {
+        // Removes the compression version from the string
+        return new File(srcFile.getParent() + File.separator + srcFile.getName().substring(VERSION.length()));
+    }
+
+    // Basic Testing
+    public static void main(String[] args) throws IOException {
+        byte[] ba = {1, 1, 1, 1, 2, 2, 7, 7, 7, 7, 7, 14, 14, 16, 17};
+        // Caso queira testar coloque o path
+        File in = new File("");
+        RandomAccessFile raf = new RandomAccessFile(in, "rw");
+        raf.write(ba);
+        HuffmanCoding h = new HuffmanCoding();
+        h.Decompress(h.compress(in));
+    }
+}
---
+++ tps/tp1/dev/Banco/estruturas/Huffman/HuffmanTree.java
@@ -0,0 +1,134 @@
+package Huffman;
+
+import java.io.*;
+import java.util.Hashtable;
+import java.util.TreeSet;
+import java.util.function.BiConsumer;
+
+/**
+ * @author Vinicius G Santos
+ */
+public class HuffmanTree {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public HuffmanTree(byte[] ba) throws IOException {
+        fromByteArray(ba);
+    }
+    public HuffmanTree(TreeSet<Node> nodePool) {
+        this.root = CreateTree(nodePool);
+    }
+
+    public HuffmanTree() {
+        this.root = null;
+    }
+
+    /**
+     * Creates a huffman tree given a set of the leaf nodes.
+     * @param nodePool A set with all the leaf nodes.
+     * @return The root node of the created Tree.
+     */
+    private Node CreateTree(TreeSet<Node> nodePool) {
+        if (nodePool == null)
+            throw new IllegalArgumentException("Parameter must not be null");
+        // Pops the first two nodes in the set
+        // creates a parent node from them and inserts it
+        // util there's only the root left
+        while (nodePool.size() > 1) {
+            nodePool.add(new Node(
+                    nodePool.pollFirst(),
+                    nodePool.pollFirst()
+                )
+            );
+        }
+        return nodePool.pollFirst();
+    }
+
+    /**
+     * Creates a Hashtable with the Occurrence value as the key of the table and
+     * the binary path of the tree in a string as the value of the table.
+     * @return A hashtable containing the occurrences (key) and their string binary encode (value).
+     */
+    public Hashtable<Byte, String> genEncodeTable() {
+        char[] path = new char[128];
+        Hashtable<Byte, String> encodeTable = new Hashtable<>();
+        consumeValPath(root, path, -1, encodeTable::put);
+        return encodeTable;
+    }
+
+    /**
+     * Creates a Hashtable with the Occurrence value as the value of the table and
+     * the binary path of the tree in a string as the Key of the table.
+     * @return A hashtable containing the occurrences (value) and their string binary encode (key).
+     */
+    public Hashtable<String, Byte> getDecodeTable() {
+        char[] path = new char[128];
+        Hashtable<String, Byte> decodeTable = new Hashtable<>();
+        consumeValPath(root, path, -1, (b, str) -> {
+            decodeTable.put(str, b);
+        });
+        return decodeTable;
+    }
+
+    /**
+     * Navigates in the tree to find the path of all occurrences and
+     * use it with a BiConsumer function
+     * @param node The node of the tree to be visited
+     * @param path A char array to trace the binary path
+     * @param len The current length used in the char array
+     * @param consumer A BiConsumer function to use the node value and its binary path
+     */
+    private void consumeValPath(Node node, char[] path, int len, BiConsumer<Byte, String> consumer) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) {
+            String aux = "";
+            for (int i = 0; i <= len; i++)
+                aux += path[i];
+            consumer.accept(node.getValue(), aux);
+        } else {
+            path[++len] = '0';
+            consumeValPath(node.left, path, len, consumer);
+            path[len] = '1';
+            consumeValPath(node.right, path, len, consumer);
+        }
+    }
+
+    /**
+     * Converts the instanced tree into a byte array
+     * containing a leaf value (byte) followed by its weight (int)
+     * @param nodePool A set with all the occurrences.
+     * @return A byte array containing the leaf node's data
+     * @throws IOException I/O error caused by DataOutputStream
+     */
+    public static byte[] toByteArray(TreeSet<Node> nodePool) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+        // Writes the occurrences value and weight in the byte array
+        for (Node n : nodePool) {
+            dos.writeByte(n.getValue());
+            dos.writeInt(n.getWeight());
+        }
+        return bos.toByteArray();
+    }
+
+    /**
+     * Populates the instanced tree with the data contained in the given byte array
+     * @param ba The given byte array with the data to be loaded
+     * @throws IOException I/O error caused by DataInputStream
+     */
+    public void fromByteArray(byte[] ba) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(ba);
+        DataInputStream dis = new DataInputStream(bis);
+        TreeSet<Node> nodePool = new TreeSet<>(Node::TreeSetCompareTo);
+        int n = (ba.length / 5);
+        // Creates a set with the leaf nodes
+        for (int i = 0; i < n; i++)
+            nodePool.add(new Node(dis.readByte(), dis.readInt()));
+        // Builds the tree with the leaf nodes
+        this.root = CreateTree(nodePool);
+    }
+
+}
